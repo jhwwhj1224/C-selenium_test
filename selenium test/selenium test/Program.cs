4318c661d1715ad12ed541cfc692417dcc33ecc6
Hee Won Jung <jhwwhj1224@gmail.com>
2019-04-15 14:33:07 -0700

    selenium test
    navigate an url then grab title

---
+++ selenium test/selenium test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace selenium_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Navigate().GoToUrl("https://stockx.com/air-jordan-1-retro-high-black-hyper-pink-2-gs");
+
+            String title = driver.Title;
+
+            Console.WriteLine(title);
+        }
+    }
+}
